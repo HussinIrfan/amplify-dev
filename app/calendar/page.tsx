d604c1ea255e654d69dee099be0e38bc79438ad7
Zanderskier <46981525+Zanderskier@users.noreply.github.com>
2025-01-29 20:23:37 -0800

    Created Calendar View options and added bg image with some minor css changes (#8)

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -1,25 +1,36 @@
 "use client";
 
 import CustomNavbar from "../CustomNavbar";
+import ContactInfoBox from "../components/ContactInfoBox";
+import Image from 'next/image';
 import "@aws-amplify/ui-react/styles.css";
 import "../page.module.css";
 import "../admin/admin.css";
 import "./index.css";
 import BasicCalendar from "./BasicCalendar";
+import sltFirePhoto from "./slt-fire-group.jpg";
 
 export default function CalendarPage() {
   return (
     <>
-      <main>
-        <div>
+      <main className="main">
           <CustomNavbar />
           <div>
-            <h1>Calendar</h1>
-          </div>
+            <Image
+            src={sltFirePhoto}
+            alt="Photo"
+            layout="responsive"
+            objectFit="cover"
+            className="background-image-div"
+            />
+            </div>
+        <div className="calendar-div">
+          <h2 className="calendar-h1">Calendar</h2>
           <div className="calendar-container">
             <BasicCalendar />
           </div>
         </div>
+        <ContactInfoBox />
       </main>
     </>
   );
