eb60b674f83fe0c643df7ffef9bea68acdfde737
HussinIrfan <112319824+HussinIrfan@users.noreply.github.com>
2025-02-05 19:56:32 -0800

    SLTFF-134
    * Made navbar responsive
    
    * Updated logo in navbar and fixed bug with white boarders
    
    * Removed old assets and added facebook link

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
 import "./../app/app.css";
@@ -10,11 +9,13 @@ import "@aws-amplify/ui-react/styles.css";
 import Link from "next/link";
 
 import CustomNavbar from "./CustomNavbar";
+
 import HPheader from "./HPheader";
 import ImageCarousel from "./ImageCarousel";
 import NewsletterPopup from "./news-letter/NewsletterPopup";
 import ContactInfoBox from "./components/ContactInfoBox";
 import ContactUs from "./components/ContactUs";
+
 Amplify.configure(outputs);
 
 const client = generateClient<Schema>();
@@ -25,14 +26,17 @@ export default function App() {
   return (
     <main>
       <CustomNavbar />
+    
       <HPheader />
       <ImageCarousel />
       <NewsletterPopup />
       <ContactUs/>
+      
       {/* Link to the login page */}
+      
       <Link href="/login">Go to Login Page</Link>
       <ContactInfoBox />
-
+     
 
       
     </main>
