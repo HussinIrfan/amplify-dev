400f083c8d47841e14b99847172db3146291618e
Anh Huynh <annthuynh98@gmail.com>
2025-02-06 20:39:47 -0800

    SLTFF-148

---
+++ app/store1/ProductDetails.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import styles from "./ProductDetails.module.css";
+
+interface ProductDetailsProps {
+    id: string;
+    title: string;
+    price: number;
+    description: string;
+    quantity: number;
+
+}
+
+const ProductDetails: React.FC<ProductDetailsProps> = ({
+    id,
+    title,
+    price,
+    description,
+    quantity
+}) => {
+    return (
+        <div className={styles['product_details']}>
+            <span className={styles["title"]}>
+                {title}
+            </span>
+
+            <span className={styles['description']}>        
+                {description}
+            </span>
+
+            <span className={styles['price']}>
+                ${price.toFixed(2)}
+            </span>
+
+            <div>
+                <span className={styles['quantity']}>
+                    Quanity: {quantity}
+                </span>
+
+                <button className={styles['btn-wrapper']}>Add to Cart</button>
+            </div>
+
+        </div>
+    )
+
+}
+
+export default ProductDetails;
