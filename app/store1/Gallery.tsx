0e77203f3bf43b23feee9c110f21da80ec817e29
Anh Huynh <annthuynh98@gmail.com>
2025-02-07 10:50:46 -0800

    added t shirt image to gallery tab and adjust css

--- app/store1/Gallery.tsx
+++ app/store1/Gallery.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import Image from 'next/image'; 
 import styles from "./Gallery.module.css";
+import productImage from "./assets/t_shirt.png"; 
 
-const Gallery = () => {
+const Gallery: React.FC = () => {
     return (
         <div className={styles['gallery']}>
-            <div className={styles['main_image']}></div>
-
+            <div className={styles['main_image']}>
+                <Image src={productImage} alt="Product" className={styles['image']} layout="intrinsic" />
+            </div>
         </div>
-
-    )
-
+    );
 }
 
 export default Gallery;
