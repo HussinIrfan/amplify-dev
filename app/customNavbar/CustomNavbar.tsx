9d7f23df4b533a8a29adbc178b110b703f97840c
calinchan <145807733+calinchan@users.noreply.github.com>
2025-02-07 20:00:44 -0800

    SLTFF-156,158

--- app/customNavbar/CustomNavbar.tsx
+++ app/customNavbar/CustomNavbar.tsx
@@ -45,7 +45,7 @@ export default function CustomNavbar() {
               <Link className={styles.navbarItem} href="#" onClick={closeMobileMenu}>
                 About Us
               </Link>
-              <Link className={styles.navbarItem} href="#" onClick={closeMobileMenu}>
+              <Link className={styles.navbarItem} href="/ourwork" onClick={closeMobileMenu}>
                 Our Work
               </Link>
               <Link className={styles.navbarItem} href="#" onClick={closeMobileMenu}>
