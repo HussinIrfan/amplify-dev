45f0212cd6e7eaf5c5b91f86ca6a8379bf90cb43
HussinIrfan <hussinirfan1@gmail.com>
2025-02-12 17:17:51 -0800

    testing with different colors on homepage

--- app/homepage/HPheader.tsx
+++ app/homepage/HPheader.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HPheader = () => {
   return (
-    <section style={{ textAlign: "center", padding: "20px 0", color: "#000", backgroundColor: "#FFFFF" }}>
+    <section style={{ textAlign: "center", padding: "20px 0", color: 'white', backgroundColor: "#FFFFF" }}>
       <h1 className="hp-header">
         THE BETTER TRAINED THEY ARE, THE SAFER OUR COMMUNITY IS
       </h1>
