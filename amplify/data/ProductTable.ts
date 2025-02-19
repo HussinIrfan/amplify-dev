6c2ab327d76285aa7232c13d6afe0b4894fc615a
Anh Huynh <annthuynh98@gmail.com>
2025-02-19 07:37:04 -0800

    change name of columns and removed a field for table schema

--- amplify/data/ProductTable.ts
+++ amplify/data/ProductTable.ts
@@ -3,7 +3,6 @@ import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 const schema = a.schema({
   Product: a
     .model({
-      productId: a.id().required(),
       name: a.string().required(),
       description: a.string(),
       price: a.float().required(),
@@ -14,11 +13,9 @@ const schema = a.schema({
 
   ProductVariant: a
     .model({
-      variantId: a.id().required(),
       productId: a.id().required(),
       size: a.string(),
-      color: a.string(),
-      stock: a.integer().required(),
+      quantity: a.integer().required(),
       product: a.belongsTo("Product", "productId"),
     })
     .authorization((allow) => [allow.publicApiKey()]),
