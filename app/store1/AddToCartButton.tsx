7e27917d0a90570ec9d47ba0a89db935707031cd
Anh Huynh <annthuynh98@gmail.com>
2025-02-22 14:14:47 -0800

    added files for add to cart button. No logic is written for now

---
+++ app/store1/AddToCartButton.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./assets/AddToCartButton.css"; // Ensure the correct path
+
+const AddToCartButton: React.FC = () => {
+  return (
+    <button className="add-to-cart-btn">
+      Add to Cart
+    </button>
+  );
+};
+
+export default AddToCartButton;
--- app/store1/page.tsx
+++ app/store1/page.tsx
@@ -4,12 +4,15 @@ import CustomNavbar from "../customNavbar/CustomNavbar";
 import ProductSection from "./ProductSection"
 import Footer from "../footer/footer";
 import styles from "./assets/page.module.css"
+import AddToCartButton from "./AddToCartButton";
 
 export default function AdminPage() {
   return (
     <div>
       <main className={styles ["main"]}>
         <CustomNavbar />
+
+        <AddToCartButton />
      
         <div className={styles ["content"]}>
           <ProductSection />
