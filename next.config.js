e6f75e1d1b45077687d6aaf8005ddcd505f56325
Armaan Randhawa <145498952+akr090@users.noreply.github.com>
2025-02-23 19:31:46 -0800

    Revert "Integrated PayPal (#47)"
    This reverts commit 6c701bf7d3b17737a0bf46ece057efc9f5f6be27.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    env: {
-      NEXT_PUBLIC_PAYPAL_CLIENT_ID: process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID,
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+const nextConfig = {}
+
+module.exports = nextConfig
